c8ee44965bdc9ef97b6b89e43a1d55f54690ea10
Tatjana <tatjana.hoser@gmx.at>
2025-10-23 13:19:36 +0200

    Erneuter Git Test mit Import

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import NavBar from "@/components/NavBar.vue";
 </script>
 
 <template>
   <header>
+    <NavBar></NavBar>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
